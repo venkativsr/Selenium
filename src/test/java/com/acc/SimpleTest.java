fb2146ee1c3603e053a4794478def52e668338b9
purvesh-jaiswal <purvesh@appliedcloudcomputing.com>
2017-02-20 13:37:23 +0530

    Web Driver into my project hopefully this time my project running

---
+++ src/test/java/com/acc/SimpleTest.java
@@ -0,0 +1,36 @@
+package com.acc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+public class SimpleTest {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void setup(){
+		 System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	
+	@After
+	public void trminate(){
+		
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void StartTest(){
+		
+		driver.navigate().to("https://www.youtube.com/");
+		
+	}
+
+}
